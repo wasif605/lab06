6ad4949638664546df37cc2722abef26d9159b47
Administrator <Administrator@DESKTOP-NEII4G1>
2026-03-08 14:16:47 +0500

    making

--- lab06/lab06.cpp
+++ lab06/lab06.cpp
@@ -1,27 +1,54 @@
+//#include <iostream>
+//using namespace std;
+//
+//class SavingsAccount {
+//    int savingBalance;
+//public:
+//    SavingsAccount(int balance = 0) : savingBalance(balance) {}
+//    static int annualInterestRate;
+//    static void setAnnualInterestRate(int x);
+//    int monthlyInterest();
+//    int displayBalance();
+//};
+//
+//int SavingsAccount::annualInterestRate = 0;
+//
+//void SavingsAccount::setAnnualInterestRate(int x) {
+//    annualInterestRate = x;
+//}
+//
+//int SavingsAccount::monthlyInterest() {
+//    return (savingBalance * annualInterestRate) / 12;
+//}
+//
+//int SavingsAccount::displayBalance() {
+//    return savingBalance + monthlyInterest();
+//}
+//
+//int main() {
+//    SavingsAccount s1(1000);
+//    SavingsAccount::setAnnualInterestRate(6);
+//
+//    cout << "Monthly Interest: " << s1.monthlyInterest() << endl;
+//    cout << "Balance after interest: " << s1.displayBalance() << endl;
+//
+//    return 0;
+//}
+
+
 #include <iostream>
 using namespace std;
 
-class SavingsAccount {
-    int savingBalance;
-    public:
-        SavingsAccount() : savingBalance(0) {}
-    static int annualInterestRate;
-    static int setAnnualInterestRate();
-    int monthlyInterest();
-    int displaybalance();
+class Calculator {
+public :
+	int x, int y;
+	Calculator() : x(0), y(0) {}
+	int addupnumbers();
 };
-static int SavingsAccount::setAnnualInterestRate(int x) {
-    annualInterestRate = x;
-}
-int SavingsAccount::monthlyInterest() {
-    return (savingBalance* annualInterestRate) / 12;
-}
-int SavingsAccount::displaybalance() {
-    return savingBalance + annualInterestRate;
+
+int Calculator::addupnumbers(int y) {
+	x = x + y;
+	return x;
 }
 
-
-int main()
-{
-    
-}
+int main
